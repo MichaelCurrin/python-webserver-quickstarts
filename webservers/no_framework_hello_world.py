5f89ea15da3cf4d1ee16cb50ad6b6261061e4cb9
Michael Currin <michael.currin@gmail.com>
2019-09-15 18:48:21 +0200

    feat: Create no framework script

---
+++ webservers/no_framework_hello_world.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+Hello World web app with no framework.
+
+Based on:
+    https://docs.python.org/3.9/library/http.server.html?highlight=base%20http
+    https://dfpp.readthedocs.io/en/latest/chapter_01.html
+
+Dedent is used to remove all common whitespace in the output.
+
+To serve the directory a files, set SimpleHTTPRequestHandler as the request
+handler rather than HelloRequestHandler.
+"""
+import textwrap
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+
+class HelloRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path != '/':
+            self.send_error(404, "Page not found")
+            return
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        response_text = textwrap.dedent("""\
+            <html>
+            <head>
+                <title>Greetings to the World</title>
+            </head>
+            <body>
+                <h1>Greetings to the World</h1>
+                <p>Hello, World!</p>
+            </body>
+            </html>
+        """)
+        self.wfile.write(response_text.encode('utf-8'))
+
+
+def run():
+    server_address = ('', 5000)
+    httpd = HTTPServer(server_address, HelloRequestHandler)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run()
