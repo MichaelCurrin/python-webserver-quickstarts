9134fccc840bcb55553cedbc4e661fc18eca7ab1
michaelcurrin <michael.currin@gmail.com>
2020-05-27 12:35:58 +0200

    Update cherrypy_hello_world.py

--- webservers/cherrypy_hello_world.py
+++ webservers/cherrypy_hello_world.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
+"""
+Hello world HTTP app using Cherrpy.
+"""
 import cherrypy
 
 
 class HelloWorld(object):
+    
     @cherrypy.expose
     def index(self):
         return "Hello World!"
 
 
 if __name__ == '__main__':
-    cherrypy.config.update({'server.socket_port': 5000})
+    cherrypy.config.update(
+        {'server.socket_port': 5000}
+    )
     cherrypy.quickstart(HelloWorld())
