b2c89949cfdf86958f48e7b3f69d1068bbbba244
michaelcurrin <michael.currin@gmail.com>
2020-05-27 12:39:51 +0200

    Update manage.py

--- webservers/django_hello_world/manage.py
+++ webservers/django_hello_world/manage.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""
+Django's command-line utility for administrative tasks.
+"""
 import os
 import sys
 
 
 def main():
+    """
+    Command-line entry-point to manage Django app.
+    """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_hello_world.settings')
+    
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as e:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        ) from e
+        
     execute_from_command_line(sys.argv)
 
 
