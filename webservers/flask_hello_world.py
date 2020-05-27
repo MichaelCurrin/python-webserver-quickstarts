819f57b0686d1cae7a734bfc1aeb3999fdd5a0cf
michaelcurrin <michael.currin@gmail.com>
2020-05-27 12:37:47 +0200

    Update flask_hello_world.py

--- webservers/flask_hello_world.py
+++ webservers/flask_hello_world.py
@@ -1,3 +1,10 @@
+"""
+Hello world HTTP app - using Flask.
+
+    
+This will pick 'name' from query parameters.
+e.g. http://localhost:5000/?name=Visitor
+"""
 from flask import Flask, escape, request
 
 app = Flask(__name__)
@@ -6,8 +13,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """
-    This will pick 'name' from query parameters.
-    e.g. http://localhost:5000/?name=Visitor
+    Handle GET request on the root path, using query parameters.
     """
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    
+    return f"Hello, {escape(name)}!"
